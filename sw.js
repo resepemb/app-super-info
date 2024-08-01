1f9ea460f5424fc350bdfe018beddfbb78e2b7df
Gabriel Café <63719840+GabrielCoffee9@users.noreply.github.com>
2024-08-01 11:55:30 -0300

    Update PWA
    injectManifest to precache & custom Service Worker.

--- sw.js
+++ sw.js
@@ -1,20 +1,82 @@
 importScripts('files/static/workbox-sw.js');
 
-const { precacheAndRoute } = workbox.precaching;
-const { setDefaultHandler } = workbox.routing;
+const { PrecacheController } = workbox.precaching;
+const { registerRoute, setDefaultHandler } = workbox.routing;
 const { CacheFirst } = workbox.strategies;
 const { clientsClaim } = workbox.core;
 
+const scope = self.registration.scope;
 
-const scope = self.registration.scope; 
+// PRECACHE INJECT MANIFEST
+var manifestList = self.__WB_MANIFEST;
 
-precacheAndRoute([
-    {url: scope+'index.html', revision: '01'},
-    {url: scope+'sobre.html', revision: '01'},
+const precacheController = new PrecacheController();
+
+precacheController.strategy.matchOptions = {
+  ignoreVary: true,
+};
+
+// Files with revision Null
+precacheController.addToCacheList([
+  {"revision": null, "url":"site_libs/bootstrap/bootstrap-icons.woff"},
+  {"revision": null, "url":"https://fonts.gstatic.com/s/inter/v18/UcC73FwrK3iLTeHuS_nVMrMxCp50SjIa1ZL7.woff2"},
+  {"revision": null, "url":" https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"},
 ]);
+
+precacheController.addToCacheList(manifestList);
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  precacheController.install(event);
+});
+
+self.addEventListener('activate', (event) => {
+  precacheController.activate(event);
+});
+
+
+// INTERNAL REQUESTS
+registerRoute(({url}) => url.href.includes(scope),async({url ,event}) => {
+  try {
+      let cacheResponse = await precacheController.matchPrecache(url.pathname);
+
+      if(cacheResponse == undefined && url.pathname === '/'){
+        cacheResponse = await precacheController.matchPrecache('/index.html');
+      }
+
+      if(cacheResponse == undefined){
+        console.debug(`No custom cached response found in ${url.pathname} Redirecting...`);
+      }
+      
+      return cacheResponse || fetch(event.request);
+  } catch (error) {
+      return new Response('Error loading internal content', { status: 500 });
+  }
+}); 
+
+// EXTERNAL REQUESTS
+registerRoute(({url}) => !url.href.includes(scope),async({url ,event}) => {
+  try {
+      let cacheResponse = await precacheController.matchPrecache(url);
+
+
+      if(cacheResponse == undefined){
+        console.debug(`No custom cached response found in ${url.pathname} Redirecting...`);
+      }
+      
+      return cacheResponse || fetch(event.request);
+  } catch (error) {
+      return new Response('Error loading external content', { status: 500 });
+  }
+}); 
 
 
 const strategy = new CacheFirst();
+
+strategy.matchOptions = {
+  ignoreVary: true,
+};
+ 
 setDefaultHandler(strategy);
 
 clientsClaim();
