3e63abb9b3ae4c68c760cf79c3bd86a52da55060
Gabriel Café <63719840+GabrielCoffee9@users.noreply.github.com>
2024-09-11 14:03:24 -0300

    Update copy_files_site.ts
    Tentativa de fazer com que a branch gh_pages possua o arquivo loadserviceworker.js

--- files/scripts/copy_files_site.ts
+++ files/scripts/copy_files_site.ts
@@ -1,8 +1,11 @@
 import { copy } from "https://deno.land/std/fs/copy.ts";
+import {ensureDir} from "https://deno.land/std/fs/ensure_dir.ts";
 
 await copy('./files/images/', './_site/files/images', { overwrite: true });
 await copy('./files/scripts/', './_site/files/scripts', { overwrite: true });
 await copy('./files/static/', './_site/files/static', { overwrite: true });
 await copy('./pwa/sw.js', './_site/sw.js', { overwrite: true });
+await ensureDir('./_site/pwa/');
+await copy('./pwa/loadserviceworker.js', './_site/pwa/loadserviceworker.js', { overwrite: true });
 
 console.log("Files copied successfully!");
